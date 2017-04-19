4bb4798f86b215c1205dedff017ac315897404fc
perry kong <apk29@hotmail.com>
2017-04-19 09:38:58 -0700

    adding routes

--- finalProject.py
+++ finalProject.py
@@ -17,18 +17,31 @@ def editRestaurant():
 
 @app.route('/restaurant/restaurant_id/delete')
 def deleteRestaurant():
-	restaurant_id = 3
+	restaurant_id = 2
 	return "This page will be for deleting restaurant %s" % restaurant_id
 
-@app.route('/restaurant/restaurant_id')
+@app.route('/restaurant/restaurant_id/')
 @app.route('/restaurant/restaurant_id/menu')
 def showMenu():
-	return "This page is the menu for restaurant %s" restaurant_id
-
-@app.route('/restaurant/restaurant_id/menu/new')
-def newMenu():
 	restaurant_id = 3
+	return "This page is the menu for restaurant %s" % restaurant_id
+
+@app.route('/restaurant/restaurant_id/menu/new/')
+def newMenuItem():
+	restaurant_id = 4
 	return "This page is for making new menu item for retaurant %s" % restaurant_id
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/edit/')
+def editMenuItem():
+	restaurant_id = 4
+	menu_id= 5
+	return "This page is for is for editing menu item %s" % menu_id	
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/delete/')
+def deleteMenuItem():
+	restaurant_id = 6
+	menu_id= 7
+	return "This page is for deleting menu item %s" % menu_id
 
 if __name__ == '__main__':
 	app.debug = True
