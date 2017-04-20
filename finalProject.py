1472623685e9a00aac83d385e27b63dcd3b6f01b
perry kong <apk29@hotmail.com>
2017-04-20 13:24:46 -0700

    fifth, adding templates, issue with render_templates

--- finalProject.py
+++ finalProject.py
@@ -9,7 +9,8 @@ restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', '
 
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'id': '1'}
+
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
@@ -25,7 +26,7 @@ def newRestaurant():
 def editRestaurant(restaurant_id):
 	# restaurant_id = 1
 	# return "This page will be for editing restaurant %s" % restaurant_id
-	return render_template('editRestaurant.html', restaurant=restaurant)
+	return render_template('editRestaurant.html', restaurants=restaurants)
 
 @app.route('/restaurant/restaurant_id/delete/')
 def deleteRestaurant():
